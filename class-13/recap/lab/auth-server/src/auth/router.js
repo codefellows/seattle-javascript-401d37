a380154fd4dae750dd13ca787d5145e6a41d57d5
JB-Tellez <jb.tellez@gmail.com>
2020-08-26 08:49:45 -0700

    adds todos to recap

--- class-13/recap/lab/auth-server/src/auth/router.js
+++ class-13/recap/lab/auth-server/src/auth/router.js
@@ -9,36 +9,16 @@ const oauthMiddleware = require('./middleware/oauth.js');
 
 authRouter.post('/signup', async (req, res, next) => {
 
-  try {
-
-    const user = await new User(req.body).save();
-
-    req.token = user.generateToken();
-
-    res.send({
-      token: req.token,
-      user: req.user,
-    });
-
-  } catch (err) {
-
-    next(err);
-  }
+  // TODO
 
 });
 
 authRouter.post('/signin', auth, (req, res, next) => {
-  res.cookie('auth', req.token);
-  res.set('token', req.token);
-
-  res.send({
-    token: req.token,
-    user: req.user,
-  });
+  // TODO
 });
 
 authRouter.get('/oauth', oauthMiddleware, (req, res, next) => {
-  res.status(200).send(req.token);
+  // TODO
 });
 
 module.exports = authRouter;
