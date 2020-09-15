96f10bf9b4453adb9e5b7db422bee0a51d583752
JB-Tellez <jb.tellez@gmail.com>
2020-09-15 12:45:02 -0700

    adds react-json-pretty

--- class-27/demo/props-and-state/src/app-star-wars.js
+++ class-27/demo/props-and-state/src/app-star-wars.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import JSONPretty from 'react-json-pretty';
 
 import Form from './form.js';
 import People from './people.js';
@@ -27,6 +28,7 @@ class App extends React.Component {
       <>
         <Form prompt="Get some Star Wars Folks..." toggleLoading={this.toggleLoading} handler={this.handleForm} />
         <People loading={this.state.loading} people={this.state.results} count={this.state.count} />
+        <JSONPretty data={this.state}></JSONPretty>
       </>
     );
   }
